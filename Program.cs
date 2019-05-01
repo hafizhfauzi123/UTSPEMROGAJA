c5821af5954db5f62c9d747b10f07d0a58955336
hafizhfauzi123 <48465464+hafizhfauzi123@users.noreply.github.com>
2019-05-01 22:04:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utspemrogaja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan krywn = new Karyawan();
+
+            krywn.nama = "HAFIZH";
+            krywn.nik = "2345";
+            krywn.gaji = 2000000;
+
+            krywn.nama2 = "DWI AJI";
+            krywn.nik2 = "2332";
+            krywn.gaji2 = 3000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utspemrogaja
+{
+    class Karyawan
+    {
+        public string nama { get; set; }
+        public string nik { get; set; }
+        public int gaji { get; set; }
+        public string nama2 { get; set; }
+        public string nik2 { get; set; }
+        public int gaji2 { get; set; }
+        public void infoGaji()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("gaji kau bulan ini.");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("No    Nik  |    Nama         |    total gaji| ");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("1    {0}  |    {1}     |     {2}  |", nik, nama, gaji);
+            Console.WriteLine("2    {0}  |    {1}     |     {2}  |", nik2, nama2, gaji2);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+        }
+        public void kenaikanGaji()
+        {
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+            else if (gaji2 < 0)
+            {
+                gaji2 = 0;
+            }
+            Console.WriteLine("ciee dapat bonus gaji :");
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("No    Nik   |   Nama         |  gaji + bonus | ");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("1     {0}  |   {1}     |     {2}   | ", nik, nama, gaji + (gaji * 0.1));
+            Console.WriteLine("2     {0}  |   {1}     |     {2}   | ", nik2, nama2, gaji2 + (gaji2 * 0.1));
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
